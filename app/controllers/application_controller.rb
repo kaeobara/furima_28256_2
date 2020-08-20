e62f352ac79d7dd5a8d6ed2061ca00799327238b
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-08-20 18:06:08 +0900

    テストコード新規登録ができない時

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname , :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day, :email, :assword,])
   end
+
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "正しい名前を入力してください"}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "正しい名前を入力してください"}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで入力してください"}
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで入力してください"}
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+    validates :email, uniqueness: true
+    validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "半角英数混合で入力してください"}
+    validates :password,confirmation: true
+    validates :password,length: { minimum: 6 } 
+    validates :password_confirmation
+    validates :birth_day
+  end
+
+
 end
