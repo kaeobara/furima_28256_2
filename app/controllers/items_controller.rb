02efffa0a167f375379a6c9a6d5d9a30f8104447
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-08-24 19:39:21 +0900

    エラーの解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit( :prefecture_id, :category_id, :item_condition_id)
+    params.require(:item).permit(:prefecture_id, :category_id, :item_condition_id, :postage_payer_id, :shipping_day_id)
   end
 
   # def set_prefecture
