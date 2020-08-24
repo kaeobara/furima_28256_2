8437e69b2b264332df3fe8c4efcea8ee8156d883
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-08-24 23:35:47 +0900

    mini_magick,activestorageの導入

--- db/migrate/20200816102553_create_items.rb
+++ db/migrate/20200816102553_create_items.rb
@@ -9,7 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :item_condition_id,           null: false      
       t.integer :postage_payer_id,            null: false      
       t.integer :prefecture_id,               null: false      
-      t.integer :shipping_days_id,            null: false      
+      t.integer :shipping_day_id,            null: false      
       t.integer :price,                       null: false      
       t.references :user,                     foreign_key: true
       t.timestamps
