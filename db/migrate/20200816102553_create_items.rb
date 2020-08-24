2e71de80e984e7599a5b5d69ff450d58e0162153
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-08-24 19:40:31 +0900

    発送日数のプルダウン表示

--- db/migrate/20200816102553_create_items.rb
+++ db/migrate/20200816102553_create_items.rb
@@ -8,7 +8,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :category_id,                 null: false      
       t.integer :item_condition_id,           null: false      
       t.integer :postage_payer_id,            null: false      
-      t.integer :shipping_region_id,          null: false      
+      t.integer :prefecture_id,               null: false      
       t.integer :shipping_days_id,            null: false      
       t.integer :price,                       null: false      
       t.references :user,                     foreign_key: true
