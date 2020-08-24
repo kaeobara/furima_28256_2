f42c4a1a1155ec79b788d6f86de572f9dd92ff28
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-08-24 16:11:06 +0900

    prefectureのプルダウンの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+
+  with_options presence: true do
+    validates :prefecture_id
+    validates :category_id
+  end
 end
+  
+# private
+#   def product_params
+#     params.require(:product)
+#       .permit(:name, :description, :price, :condition, :brand, :send_price,
+#               :ship_day, images_attributes: [:name])
+#       .merge(user_id: current_user.id, category_id: params[:product][:category_id],
+#             prefecture_id: params[:product][:prefecture_id], status: 0)
+#end
