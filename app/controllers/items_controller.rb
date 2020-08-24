21fa06ce6802b707358a4c121a4ee296e7643b85
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-08-24 16:42:03 +0900

    カテゴリーのプルダウンの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,22 @@
 class ItemsController < ApplicationController
+  
+
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit( :prefecture_id, :category_id)
+  end
+
+  # def set_prefecture
+  #   @prefecture = Prefecture.find(@item.prefecture_id)
+  # end
 
 
 
