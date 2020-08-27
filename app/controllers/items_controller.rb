08fe8c9a032f585f564857207be651621a918cd1
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-08-28 03:04:20 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,7 @@ class ItemsController < ApplicationController
     end
   end
 
+<<<<<<< Updated upstream
   def show
     @item = Item.find(params[:id])
   end
@@ -29,12 +30,31 @@ class ItemsController < ApplicationController
   def updata
   end
  
+=======
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+>>>>>>> Stashed changes
 
 
 
   private
   def item_params
     params.require(:item).permit(:prefecture_id, :category_id, :item_condition_id, :postage_payer_id, :shipping_day_id, :image, :name, :introduction, :price).merge(user_id: current_user.id)
+  end
+
+  def ensure_current_user
+    item = Item.find(params[:id])
+    if item.user_id != current_user.id
+      redirect_to action: :index
+    end
   end
 
   # def set_prefecture
