e0688ab785a0c91cc228c70b876c8884eb1e942e
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-08-29 17:54:23 +0900

    show,edit,updateの記述をbeforeactionでまとめた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: [:index, :show]
-
-
+  before_action :find_params, only: [:show, :edit, :update]
+  
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day, :email, :assword])
+  end
+
+  def find_params
+    @item = Item.find(params[:id])
   end
 end
